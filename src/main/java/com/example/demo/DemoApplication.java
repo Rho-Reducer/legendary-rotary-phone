2aa85921b19a207e75727a6ba6384f1f61974da3
Fred Luke <fredricluke2@gmail.com>
2023-03-18 18:40:38 -0400

    Removing generated source and updating build.gradle to reflect change. It is up to the dev to add the generated source directories to the IDE classpath for debugging

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DemoApplication {
--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -1,25 +1,30 @@
 package com.example.demo.entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NonNull;
 
 @Entity
-//@Table(name="Articles")
+@Table(name="Articles")
 @Data
 public class Article {
   
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+//  @GeneratedValue(strategy=GenerationType.AUTO)
+  @Column
   private long id;
 
+  @Column
   @NonNull
   private String title;
   
+  @Column
   @NonNull
   private String content;
   
